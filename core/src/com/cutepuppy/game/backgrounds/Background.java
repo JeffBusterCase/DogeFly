7fc078623939b8467d1682e61cfa04768edb3073
Jefferson David Silva Bomfim <jefferson_bomfim@outlook.com>
2016-12-15 15:51:53 -0200

    Now argument must not be a Array

--- core/src/com/cutepuppy/game/backgrounds/Background.java
+++ core/src/com/cutepuppy/game/backgrounds/Background.java
@@ -8,8 +8,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
  * Created by jeffbustercase on 09/12/16.
  */
 public class Background extends Image {
-    public Background(Texture[] textures) {
-        super(textures[0]);
+    public Background(Texture texture) {
+        super(texture);
         // TODO: uses animation. so interate over textures
 
         setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
