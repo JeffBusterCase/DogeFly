9d1cf84df8da495a6cfaeb74584822ae867a935d
Jefferson David Silva Bomfim <jefferson_bomfim@outlook.com>
2016-12-15 15:43:00 -0200

    Assets now are loaded by level

--- core/src/com/cutepuppy/game/open/AssetManager.java
+++ core/src/com/cutepuppy/game/open/AssetManager.java
@@ -6,41 +6,36 @@ import com.badlogic.gdx.graphics.Texture;
  * Created by jeffbustercase on 15/12/16.
  */
 public class AssetManager extends com.badlogic.gdx.assets.AssetManager {
-    public static final Texture DogeTexture = new Texture("./assets/player/doge.png");
-    public static final Texture HarpoonTexture = new Texture("./assets/harpoon.png");
-    public static final Texture ExplosionTexture = new Texture("./assets/explosion.png");
-    public static final Texture BirdTexture = new Texture("./assets/bird/bird-f1.png");
-    public static final Texture FalconTexture = new Texture("./assets/falcon/falcon-f1.png");
-    public static final Texture ButtonStartTexture = new Texture("./assets/buttons/start.png");
-    public static final Texture ButtonNextLevelTexture = new Texture("./assets/buttons/nextLevel.png");
-    public static final Texture ButtonMenuTexture = new Texture("./assets/buttons/menu.png");
-    public static final Texture ButtonRetryTexture = new Texture("./assets/buttons/retry.png");
-    public static final Texture ButtonQuitTexture = new Texture("./assets/buttons/quit.png");
-    public static final Texture[] BackgroundTextures = {new Texture("./assets/backgrounds/gameBackground.png")};
-    public static final Texture[] SkyBackgroundTextures = {new Texture("./assets/backgrounds/skyBackgroundTexture.png")};
-    public static final Texture[] MenuBackgroundTextures = {new Texture("./assets/backgrounds/menuBackground.png")};
-    public static final Texture[] EndGameBackgroundTextures = {new Texture("./assets/backgrounds/endGameBackground.png")};
-    public static final Texture[] WinBackgroundTextures = {new Texture("./assets/backgrounds/winGameBackground.png")};
     public AssetManager() {
         super();
-
-        // TODO: Organizar dentro dos apropriados metodos
-
-
+    }
+    public void loadDefaultGameAssets(){
+        load("player/doge.png", Texture.class);
+        load("harpoon.png", Texture.class);
+        load("explosion.png", Texture.class);
     }
     public void loadMainMenuAssets(){
-
+        load("backgrounds/menuBackground.png", Texture.class);
+        load("buttons/start.png", Texture.class);
+        load("buttons/quit.png", Texture.class);
     }
     public void loadLevel1Assets(){
-
+        load("bird/bird-f1.png", Texture.class);[
+        load("backgrounds/gameBackground.png", Texture.class);
     }
     public void loadLevel2Assets(){
-
+        load("bird/bird-f1.png");
+        load("falcon/falcon-f1.png", Texture.class);
+        load("backgrounds/skyBackgroundTexture.png", Texture.class);
     }
     public void loadLostGameAssets(){
-
+        load("buttons/menu.png");
+        load("buttons/retry.png");
+        load("backgrounds/endGameBackground.png", Texture.class);
     }
     public void loadWinGameAssets(){
-
+        load("buttons/nextLevel.png", Texture.class);
+        load("buttons/menu.png", Texture.class);
+        load("backgrounds/winGameBackground.png", Texture.class);
     }
 }
